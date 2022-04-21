28055d4ace64c3ce60c77f8219bc48b6c5bef9af
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-04-21 09:51:14 +0000

    ✅ add service component

---
+++ test/src/components/Home/service.js
@@ -0,0 +1,115 @@
+import React from 'react';
+
+const Service = () => {
+  return (
+    <div>
+  
+<section id="service" class="pattern-style-4 has-overlay">
+    <div class="container raise-2">
+        <h6 class="section-subtitle text-center">Featured Food</h6>
+        <h3 class="section-title mb-6 pb-3 text-center">Special Dishes</h3>
+        <div class="row">
+            <div class="col-md-6 mb-4">
+                <a href="javascrip:void(0)" class="custom-list">
+                    <div class="img-holder">
+                        <img src="assets/imgs/dish-1.jpg" />
+                    </div>
+                    <div class="info">
+                        <div class="head clearfix">
+                            <h5 class="title float-left">Aperiam incidunt dicta</h5>
+                            <p class="float-right text-primary">$25</p>
+                        </div>
+                        <div class="body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing Eos, earum dicta est veniam beatae libero!</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6 mb-4">
+                <a href="javascrip:void(0)" class="custom-list">
+                    <div class="img-holder">
+                        <img src="assets/imgs/dish-2.jpg" />
+                    </div>
+                    <div class="info">
+                        <div class="head clearfix">
+                            <h5 class="title float-left">Facere molestiae quaerat</h5>
+                            <p class="float-right text-primary">$35</p>
+                        </div>
+                        <div class="body">
+                            <p>Voluptatem voluptate ad fugit aliquam, laboriosam neque vero incidunt itaque.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6 mb-4">
+                <a href="javascrip:void(0)" class="custom-list">
+                    <div class="img-holder">
+                        <img src="assets/imgs/dish-3.jpg" />
+                    </div>
+                    <div class="info">
+                        <div class="head clearfix">
+                            <h5 class="title float-left">Veniam Beatae Libero</h5>
+                            <p class="float-right text-primary">$18</p>
+                        </div>
+                        <div class="body">
+                            <p>Accusamus libero quo tempore suscipit molestias qui quam illo facere aspernatur esse! Doloribus?</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6 mb-4">
+                <a href="javascrip:void(0)" class="custom-list">
+                    <div class="img-holder">
+                        <img src="assets/imgs/dish-4.jpg" />
+                    </div>
+                    <div class="info">
+                        <div class="head clearfix">
+                            <h5 class="title float-left">Incidunt Eius</h5>
+                            <p class="float-right text-primary">$30</p>
+                        </div>
+                        <div class="body">
+                            <p>Doloremque maiores tempore, nostrum rerum nihil distinctio expedita voluptates eos deserunt.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6 mb-4">
+                <a href="javascrip:void(0)" class="custom-list">
+                    <div class="img-holder">
+                        <img src="assets/imgs/dish-5.jpg" />
+                    </div>
+                    <div class="info">
+                        <div class="head clearfix">
+                            <h5 class="title float-left">Nihil dDstinctio</h5>
+                            <p class="float-right text-primary">$24</p>
+                        </div>
+                        <div class="body">
+                            <p>Doloremque maiores tempore, nostrum rerum expedita incidunt eius voluptates eos deserunt.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6 mb-4">
+                <a href="javascrip:void(0)" class="custom-list">
+                    <div class="img-holder">
+                        <img src="assets/imgs/dish-6.jpg" />
+                    </div>
+                    <div class="info">
+                        <div class="head clearfix">
+                            <h5 class="title float-left">Dignissimos quidem</h5>
+                            <p class="float-right text-primary">$44</p>
+                        </div>
+                        <div class="body">
+                            <p>Doloremque maiores tempore, nostrum rerum nihil distinctio expedita voluptates eos deserunt.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>                  
+    </div>
+</section>
+    </div>
+  );
+}
+
+export default Service;
